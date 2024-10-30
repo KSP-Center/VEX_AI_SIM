52843df14493833bc745e69a92f5dfad2e69ddb9
KSP-Center <jyanzick08@gmail.com>
2024-10-29 21:04:41 -0500

    Added 'build' files for controller

---
+++ robot_controller/build/release/robot_controller.d
@@ -0,0 +1,17 @@
+build/release/robot_controller.o: robot_controller.cpp include/devices.h \
+ include/libraries.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Camera.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/types.h \
+ C:/Program\ Files/Webots/include/controller/c/webots/camera_recognition_object.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Emitter.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/GPS.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Node.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Field.hpp \
+ C:/Program\ Files/Webots/include/controller/c/webots/contact_point.h \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Receiver.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Supervisor.hpp \
+ C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
+ include/test.cpp include/robotFunctions.cpp include/simFunctions.cpp \
+ include/neural-network.cpp include/resetFunctions.cpp
