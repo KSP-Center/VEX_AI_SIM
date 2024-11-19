7c1339a5f9d57051f58ecbac61a3cca66d4b8126
KSP-Center <jyanzick08@gmail.com>
2024-11-18 20:15:39 -0600

    Updated resetFunctions.cpp

--- robot_controller/include/resetFunctions.cpp
+++ robot_controller/include/resetFunctions.cpp
@@ -1,4 +1,4 @@
-#include "include/libraries.h"
+#include "include/rewardFunctions.cpp"
 
 #define BLUEZ1 0.0705
 #define BLUEZ2 0.094
@@ -7,153 +7,97 @@
 
 class Reset{
 private:
-  // id's for red rings #1
-  void translateRingPosition1(int ID_, const double translate[3]) {
-    switch (ID_) {
-      case 1:
-        translateRedRing1->setSFVec3f(translate);
-      break;
-      case 2:
-        translateRedRing2->setSFVec3f(translate);
-      break;
-      case 3:
-        translateRedRing3->setSFVec3f(translate);
-      break;
-      case 4:
-        translateRedRing4->setSFVec3f(translate);
-      break;
-      case 5:
-        translateRedRing5->setSFVec3f(translate);
-      break;
-      case 6:
-        translateRedRing6->setSFVec3f(translate);
-      break;
-      case 7:
-        translateRedRing7->setSFVec3f(translate);
-      break;
-      case 8:
-        translateRedRing8->setSFVec3f(translate);
-      break;
-      case 9:
-        translateRedRing9->setSFVec3f(translate);
-      break;
-      case 10:
-        translateRedRing10->setSFVec3f(translate);
-      break;
-      case 11:
-        translateRedRing11->setSFVec3f(translate);
-      break;
-      case 12:
-        translateRedRing12->setSFVec3f(translate);
-      break;
-    }
-  }
-  
-  // id's for red rigns #2
-  void translateRingPosition2(int ID_, const double translate[3]) {
-    switch (ID_) {
-      case 13:
-        translateRedRing13->setSFVec3f(translate);
-      break;
-      case 14:
-        translateRedRing14->setSFVec3f(translate);
-      break;
-      case 15:
-        translateRedRing15->setSFVec3f(translate);
-      break;
-      case 16:
-        translateRedRing16->setSFVec3f(translate);
-      break;
-      case 17:
-        translateRedRing17->setSFVec3f(translate);
-      break;
-      case 18:
-        translateRedRing18->setSFVec3f(translate);
-      break;
-      case 19:
-        translateRedRing19->setSFVec3f(translate);
-      break;
-      case 20:
-        translateRedRing20->setSFVec3f(translate);
-      break;
-      case 21:
-        translateRedRing21->setSFVec3f(translate);
-      break;
-      case 22:
-        translateRedRing22->setSFVec3f(translate);
-      break;
-      case 23:
-        translateRedRing23->setSFVec3f(translate);
-      break;
-    }
-  }
-
   // reset the red rings
   void specificRed(int ID, float x, float y, float z) {
     const double translateRedRing[3] = {x, y, z};
-    if (ID >= 13) {
-      translateRingPosition2(ID, translateRedRing);
-    } else {
-      translateRingPosition1(ID, translateRedRing);
+    if (ID == 1) {
+      translateRedRing1->setSFVec3f(translateRedRing);
+    } else if (ID == 2) {
+      translateRedRing2->setSFVec3f(translateRedRing);
+    } else if (ID == 3) {
+      translateRedRing3->setSFVec3f(translateRedRing);
+    } else if (ID == 4) {
+      translateRedRing4->setSFVec3f(translateRedRing);
+    } else if (ID == 5) {
+      translateRedRing5->setSFVec3f(translateRedRing);
+    } else if (ID == 6) {
+      translateRedRing6->setSFVec3f(translateRedRing);
+    } else if (ID == 7) {
+      translateRedRing7->setSFVec3f(translateRedRing);
+    } else if (ID == 8) {
+      translateRedRing8->setSFVec3f(translateRedRing);
+    } else if (ID == 9) {
+      translateRedRing9->setSFVec3f(translateRedRing);
+    } else if (ID == 10) {
+      translateRedRing10->setSFVec3f(translateRedRing);
+    } else if (ID == 11) {
+      translateRedRing11->setSFVec3f(translateRedRing);
+    } else if (ID == 12) {
+      translateRedRing12->setSFVec3f(translateRedRing);
+    } else if (ID == 13) {
+      translateRedRing13->setSFVec3f(translateRedRing);
+    } else if (ID == 14) {
+      translateRedRing14->setSFVec3f(translateRedRing);
+    } else if (ID == 15) {
+      translateRedRing15->setSFVec3f(translateRedRing);
+    } else if (ID == 16) {
+      translateRedRing16->setSFVec3f(translateRedRing);
+    } else if (ID == 17) {
+      translateRedRing17->setSFVec3f(translateRedRing);
+    } else if (ID == 18) {
+      translateRedRing18->setSFVec3f(translateRedRing);
+    } else if (ID == 19) {
+      translateRedRing19->setSFVec3f(translateRedRing);
+    } else if (ID == 20) {
+      translateRedRing20->setSFVec3f(translateRedRing);
+    } else if (ID == 21) {
+      translateRedRing21->setSFVec3f(translateRedRing);
+    } else if (ID == 22) {
+      translateRedRing22->setSFVec3f(translateRedRing);
+    } else if (ID == 23) {
+      translateRedRing23->setSFVec3f(translateRedRing);
     }
   }
   
   // reset the blue rings
   void specificBlue(int ID, float x, float y, float z) {
     const double translateBlueRing[3] = {x, y, z};
-    switch (ID) {
-      case 1:
-        translateBlueRing1->setSFVec3f(translateBlueRing);
-      break;
-      case 2:
-        translateBlueRing2->setSFVec3f(translateBlueRing);
-      break;
-      case 3:
-        translateBlueRing3->setSFVec3f(translateBlueRing);
-      break;
-      case 4:
-        translateBlueRing4->setSFVec3f(translateBlueRing);
-      break;
-      case 5:
-        translateBlueRing5->setSFVec3f(translateBlueRing);
-      break;
-      case 6:
-        translateBlueRing6->setSFVec3f(translateBlueRing);
-      break;
-      case 7:
-        translateBlueRing7->setSFVec3f(translateBlueRing);
-      break;
-      case 8:
-        translateBlueRing8->setSFVec3f(translateBlueRing);
-      break;
-      case 9:
-        translateBlueRing9->setSFVec3f(translateBlueRing);
-      break;
-      case 10:
-        translateBlueRing10->setSFVec3f(translateBlueRing);
-      break;
+    if (ID == 1) {
+      translateBlueRing1->setSFVec3f(translateBlueRing);
+    } else if (ID == 2) {
+      translateBlueRing2->setSFVec3f(translateBlueRing);
+    } else if (ID == 3) {
+      translateBlueRing3->setSFVec3f(translateBlueRing);
+    } else if (ID == 4) {
+      translateBlueRing4->setSFVec3f(translateBlueRing);
+    } else if (ID == 5) {
+      translateBlueRing5->setSFVec3f(translateBlueRing);
+    } else if (ID == 6) {
+      translateBlueRing6->setSFVec3f(translateBlueRing);
+    } else if (ID == 7) {
+      translateBlueRing7->setSFVec3f(translateBlueRing);
+    } else if (ID == 8) {
+      translateBlueRing8->setSFVec3f(translateBlueRing);
+    } else if (ID == 9) {
+      translateBlueRing9->setSFVec3f(translateBlueRing);
+    } else if (ID == 10) {
+      translateBlueRing10->setSFVec3f(translateBlueRing);
     }
   }
   
   // reset the yellow goals
   void specificYellow(int ID, float x, float y, float z) {
     const double translateGoal[3] = {x, y, z};
-    switch (ID) {
-      case 1:
-        translateGoal1->setSFVec3f(translateGoal);
-      break;
-      case 2:
-        translateGoal2->setSFVec3f(translateGoal);
-      break;
-      case 3:
-        translateGoal3->setSFVec3f(translateGoal);
-      break;
-      case 4:
-        translateGoal4->setSFVec3f(translateGoal);
-      break;
-      case 5:
-        translateGoal5->setSFVec3f(translateGoal);
-      break;
+    if (ID == 1) {
+      translateGoal1->setSFVec3f(translateGoal);
+    } else if (ID == 2) {
+      translateGoal2->setSFVec3f(translateGoal);
+    } else if (ID == 3) {
+      translateGoal3->setSFVec3f(translateGoal);
+    } else if (ID == 4) {
+      translateGoal4->setSFVec3f(translateGoal);
+    } else if (ID == 5) {
+      translateGoal5->setSFVec3f(translateGoal);
     }
   }
 
@@ -162,7 +106,7 @@ public:
     const double translateBot[3] = {x, y, z};
     const double rotateBot[4] = {x1, y1, z1, rad};
     translateFieldBot->setSFVec3f(translateBot);
-    rotateFieldBot->setSFVec3f(rotateBot);
+    rotateFieldBot->setSFRotation(rotateBot);
   }
   
   void resetRed() {
@@ -205,10 +149,10 @@ public:
   }
   
   void resetYellow() {
-    specificYellow(1, 0, -1.18, YELLOWZ);
-    specificYellow(2, 0.6, -1.49, YELLOWZ);
-    specificYellow(3, 0.6, 1.18, YELLOWZ);
-    specificYellow(4, -0.6, 1.18, YELLOWZ);
-    specificYellow(5, -0.6, -1.49, YELLOWZ);
+    specificYellow(1, 0, -1.2, YELLOWZ);
+    specificYellow(2, 0.6, -1.5, YELLOWZ);
+    specificYellow(3, 0.6, 1.2, YELLOWZ);
+    specificYellow(4, -0.6, 1.2, YELLOWZ);
+    specificYellow(5, -0.6, -1.5, YELLOWZ);
   }
 };
